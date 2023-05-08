d39a2df6d12120ffacaf0d12c69b633bdf6c63fd
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-05-08 11:55:23 +0200

    added functionality to get movie by id

--- routes/movies.js
+++ routes/movies.js
@@ -5,8 +5,22 @@ const mockData = require('../mockData');
 
 let movies = mockData;
 
+// movies is set in the endpoint in the index file
 router.get('/', (req, res) => {
   res.json(movies);
+});
+
+router.get('/:id', (req, res) => {
+  const movieID = req.params.id;
+  const movie = movies.find((m) => m.imdbID === movieID);
+
+  if (!movies) {
+    return res.status(404).json({
+      message: 'No movie with this id was found, please try an other!',
+    });
+  }
+
+  res.json(movie);
 });
 
 module.exports = router;
