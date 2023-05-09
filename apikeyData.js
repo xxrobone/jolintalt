d53b259704dc77f7780e98d34917f3f339ada7d6
user.name "robonexx <user.email "robertwagar@gmail.com>
2023-05-09 09:36:10 +0200

    added apikeyData, and a function to check if key exists in an array of apikeys, if it exists sending the movie data, works in postman

---
+++ apikeyData.js
@@ -0,0 +1,32 @@
+const apikeyData = [
+  {
+    user1: 'user1',
+    apikey: '001',
+  },
+  {
+    user2: 'user2',
+    apikey: '002',
+  },
+  {
+    user3: 'user3',
+    apikey: '003',
+  },
+  {
+    user4: 'user4',
+    apikey: '123',
+  },
+  {
+    user104: 'user104',
+    apikey: '456',
+  },
+  {
+    user202: 'user202',
+    apikey: '789',
+  },
+  {
+    user555: 'user555',
+    apikey: '100',
+  },
+];
+
+module.exports = apikeyData;
--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const movies = require('./routes/movies');
-
+const apikeysArray = require('./apikeyData');
 const app = express();
 
 const PORT = 5500;
 
 app.use(express.json());
+
+console.log(apikeysArray);
+
+// checking if api key is valid
+const authCheck = (req, res, next) => {
+  const apikey = req.query.apikey;
+  console.log(req.query);
+
+  // checking the key function
+  const checkApiKey = (obj) => obj.apikey === apikey;
+
+  // logging the api key to check if it exists in the array
+  console.log(apikeysArray.some(checkApiKey));
+
+  if (!apikey) {
+    return res
+      .status(401)
+      .send({ message: "Can't connect to server, No api key", code: 401 });
+  }
+
+  // using the checkApiKey function to check if obj key value exists in the key array
+  if (apikeysArray.some(checkApiKey)) {
+    next();
+  } else {
+    return res.status(403).send({ message: 'invalid api key' });
+  }
+};
+
+app.use((req, res, next) => {
+  authCheck(req, res, next);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello there are we up and running?!');
